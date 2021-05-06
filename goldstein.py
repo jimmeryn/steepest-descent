0135fcfca116006bbf6fc80673fef39818a4d289
DawidKahla <dawid.kahla@onet.pl>
2021-05-07 00:59:49 +0200

    Added diff in direction
    This is the first step of Goldstein algorythm

--- goldstein.py
+++ goldstein.py
@@ -1,5 +1,5 @@
-from sympy import Derivative, lambdify, symbols, sin, cos
-from numpy import zeros, matrix, arange
+import sympy as sp
+from numpy import array
 
 # Wstępnie rozpisze goldsteina jako pojedyncza funkcje i potem ewentualnie to sie jakos rozbije
 # bo jeszcze sam do konca nie jestem pewien jak to powinno dzialac
@@ -13,21 +13,69 @@ from numpy import zeros, matrix, arange
 
 
 def goldstein(start, d, beta, step, epsilon, function):
-    problem_size = start.size  # ilość zmiennych uzależniam od wymiaru punktu startowego, co może nie być najlepszym pomysłem
+    problem_size = sp.shape(start)[0]  # ilość zmiennych uzależniam od wymiaru punktu startowego, co może nie być najlepszym pomysłem
+    if problem_size < 2:
+        print("Błąd, za mało zmiennych.")
+    if problem_size > 5:
+        print("Błąd, za dużo zmiennych.")
     grad = []
     variables = []
-    x1, x2, x3 = symbols('x1 x2 x3')
-    fun = cos(x1) + 2*x2**2 - 3*x3
+    x1, x2, x3 = sp.symbols('x1 x2 x3')
+    fun = sp.cos(x1) + 2*x2**2 - 3*x3
     for i in range(1, problem_size+1):
-        variables.append(symbols('x'+str(i)))
+        variables.append(sp.symbols('x'+str(i)))
     for i in range(0, problem_size):
-       grad.append(Derivative(fun, variables[i], evaluate=True))  # takie liczenie gradientu powinno zadzialac
+        grad.append(sp.Derivative(fun, variables[i], evaluate=True))  # Tworzę listę z kolejnych pochodnych cząstkowych
+    grad_x = []
 
-    d = matrix(d).T  # żeby wymnożyć zamieniam kierunek na wektor kolumnowy, bo wstepnie zakladam, ze bedziemy go dawać wierszowo
-    f = lambdify(variables, fun)  # to zamienia ten wzorek z ładnymi zmiennymi, na taki zrozumiały dla pythona
+    # uwaga tutaj na pewno istnieje prostsze rozwiązanie w pętli, którego próba implementacji w moim wykonaniu nie przyniosła efektu
+    for j in range(0, problem_size):
+        grad_x.append(str(grad[j]))
+    grad01 = 0
+    grad02 = 0
+    grad03 = 0
+    grad04 = 0
+    grad05 = 0
 
+    # find szuka w stringu kolejnych zmiennych x1, x2 ... pewnie analogicznie trzeba bedzie napisac interpreter
+    # subs podstawia pod dana zmienna dana wartosc
+    # evalf() zapisuje wynik numerycznie zamiast sin(3.14) daje okolo 0
+
+    for i in range(1, problem_size + 1):
+        if grad_x[0].find('x' + str(i)) > 0:
+            grad01 += grad[0].subs('x' + str(i), start[i-1]).evalf()
+    for i in range(1, problem_size + 1):
+        if grad_x[1].find('x' + str(i)) > 0:
+            grad02 += grad[1].subs('x' + str(i), start[i - 1]).evalf()
+    if problem_size > 2:
+        for i in range(1, problem_size + 1):
+            if grad_x[2].find('x' + str(i)) > 0:
+                grad03 += grad[2].subs('x' + str(i), start[i - 1]).evalf()
+    if problem_size > 3:
+        for i in range(1, problem_size + 1):
+            if grad_x[3].find('x' + str(i)) > 0:
+                grad04 += grad[3].subs('x' + str(i), start[i - 1]).evalf()
+    if problem_size > 4:
+        for i in range(1, problem_size + 1):
+            if grad_x[4].find('x' + str(i)) > 0:
+                grad05 += grad[4].subs('x' + str(i), start[i - 1]).evalf()
+
+    if problem_size == 2:
+        grad0 = sp.Matrix([grad01, grad02])
+    if problem_size == 3:
+        grad0 = sp.Matrix([grad01, grad02, grad03])
+    if problem_size == 4:
+        grad0 = sp.Matrix([grad01, grad02, grad03, grad04])
+    if problem_size == 5:
+        grad0 = sp.Matrix([grad01, grad02, grad03, grad04, grad05])
+
+    p = grad0.T*d  # p jest skalarem, ale w sympy wynik mnożenia macierzy zawsze jest macierzą, więc to przypisanie jest
+    p = p[0]       #  konieczne dla zamierzonego efektu, tu można też dodać obsługę błędu, gdyby p jednak nie było skalarem
+                   # otrzymane p jest pochodną w kierunku i jej obliczenie, to pierwszy krok w Goldsteinie
+    #print(problem_size)
+    print(grad0.T)
     print(d)
-    print(f(3.14,1,0))
-    print(fun)
-    print(grad)
-    print(variables)
+    print(p)
+
+
+    #print(variables_for_grad_x1)
--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 from mns import MNS
 from goldstein import goldstein
+import sympy as sp
 
 def main():
     # TEST
     # mns = MNS()
     # mns.test()
 
-    goldstein(np.array([0, 0, 0]), np.array([1, 0, 0]), 0, 0, 0, 0)
+    goldstein(sp.Matrix([3, 2, 1]), sp.Matrix([1, 0, 0]), 0, 0, 0, 0)
 
 
 if __name__ == "__main__":
