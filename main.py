6ed2bd95331b005e8dde87b7d6c9a1ed7d4eae41
jimmeryn <jimmeryn@gmail.com>
2021-05-07 12:30:12 +0200

    Added imput equation parser

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ import numpy as np
 from mns import MNS
 from goldstein import goldstein
 import sympy as sp
+from Equation import Equation
+
 
 def main():
     # TEST
     # mns = MNS()
     # mns.test()
-
+    
+    eq = Equation();
+    print(eq.formula)
+    print(eq.variables)
     goldstein(sp.Matrix([3, 2, 1]), sp.Matrix([1, 0, 0]), 0, 0, 0, 0)
 
 
