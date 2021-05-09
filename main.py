fa8409e9533eb6623aca754cf80b9da5691c3b4c
DawidKahla <dawid.kahla@onet.pl>
2021-05-10 00:22:37 +0200

    Steepest-descent algorythm added
    It doesnt works every time.

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
 from goldstein import goldstein
 import sympy as sp
-
+from testFunctions import modified_himmelblau_function, geem_function, test_function
+from mns import mns
 
 def main():
-    # nie wiem co zrobilem źle, tutaj przy tauR = 3.5 znajduje wynik po 12 iteracjach, a dla tauR = 3.7 nie znajduje
-    goldstein(sp.Matrix([0, 0]), sp.Matrix([1, 0]), 2 / 5, 3.6, 0.001, "atan(x1)+x2**2")
+    # TEST
+    # mns = MNS()
+    # mns.test()
+    
+    #eq = Equation();
+    #print(eq.formula)
+    #print(eq.variables)
 
+    # goldstein(sp.Matrix([0, 0]), sp.Matrix([1, 0]), 2/5, 1, 0.001, test_function())
+    mns(sp.Matrix([4, 8]), 0.0001, 1000, 1/4, 3, modified_himmelblau_function())
 
 if __name__ == "__main__":
     main()
--- mns.py
+++ mns.py
@@ -1,13 +1,62 @@
-import numpy as np
+import sympy as sp
+from goldstein import goldstein
 
 
-class MNS:
-    def __init__(self):
-        self.alpha = 1  # mnożnik antygradientu
-        self.start = np.array([0, 0])  # punkt startowy algorytmu
-        self.epsilon = 1e-3  # błąd potrzebny do kryteriów stopu mniejszy lub równy 10^(-3)
+def mns(start, epsilon, L, beta, tau0, function):
+    fun = sp.parse_expr(function)
+    xk = start
+    problem_size = sp.shape(start)[0]
+    variables = []
+    grad = []
 
-    def test(self):
-        print(self.alpha)
-        print(self.start)
-        print(self.epsilon)
+    for i in range(1, problem_size + 1):
+        variables.append(
+            sp.symbols("x" + str(i))
+        )
+    replacements0 = [("x" + str(i), start[i - 1]) for i in range(1, problem_size + 1)]
+    tau = tau0
+    for k in range(1, L+1):
+
+        for i in range(0, problem_size):
+            grad.append(sp.Derivative(fun, variables[i], evaluate=True))
+
+        grad03 = 0
+        grad04 = 0
+        grad05 = 0
+        if k > 1:
+            replacements0 = [("x" + str(i), xk[i - 1]) for i in range(1, problem_size + 1)]
+
+        grad01 = grad[0].subs(replacements0).evalf()
+        grad02 = grad[1].subs(replacements0).evalf()
+        if problem_size > 2:
+            grad03 = grad[2].subs(replacements0).evalf()
+        if problem_size > 3:
+            grad04 = grad[3].subs(replacements0).evalf()
+        if problem_size > 4:
+            grad05 = grad[4].subs(replacements0).evalf()
+
+        if problem_size == 2:
+            grad0 = sp.Matrix([grad01, grad02])
+        if problem_size == 3:
+            grad0 = sp.Matrix([grad01, grad02, grad03])
+        if problem_size == 4:
+            grad0 = sp.Matrix([grad01, grad02, grad03, grad04])
+        if problem_size == 5:
+            grad0 = sp.Matrix([grad01, grad02, grad03, grad04, grad05])
+
+        scalar_product = (grad0.T * grad0)[0]
+        if scalar_product <= epsilon:
+            print("Udało się znaleźć minimum funkcji w punkcie " + str(xk) + " po " + str(k) + " iteracjach.")
+            return xk
+
+        dk = -grad0
+        print("kierunek: " + str(dk))
+        tau  = goldstein(xk, dk, beta, tau, epsilon, function)
+        xk += tau*dk
+        k += 1
+        print("Iteracja: " + str(k-1) + ", Punkt: " + str(xk))
+
+    print("Nie udało się znaleźć minimum w " + str(k) + " iteracjach.")
+    print(xk)
+    return 0
+
