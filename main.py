12349ec8cc811c824c8daa7e0230882720dd3dbc
jimmeryn <jimmeryn@gmail.com>
2021-05-10 12:32:08 +0200

    choose formula from terminal

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
-import matplotlib.pyplot as plt
-import numpy as np
+from getFormulaString import getFormulaString
+from visualisation import visualisation
 from goldstein import goldstein
 import sympy as sp
-from testFunctions import modified_himmelblau_function, geem_function, test_function
 from mns import mns
+from functionStringParser import function_string_parser
+
 
 def main():
-    # TEST
-    # mns = MNS()
-    # mns.test()
-    
-    #eq = Equation();
-    #print(eq.formula)
-    #print(eq.variables)
+    formula_string = getFormulaString()
+    # goldstein(sp.Matrix([0, 0]), sp.Matrix([1, 0]), 2/5, 1, 0.001, formula_string)
+    mns(sp.Matrix([4, 8]), 0.0001, 1000, 1 / 4, 3, formula_string)
 
-    # goldstein(sp.Matrix([0, 0]), sp.Matrix([1, 0]), 2/5, 1, 0.001, test_function())
-    mns(sp.Matrix([4, 8]), 0.0001, 1000, 1/4, 3, modified_himmelblau_function())
+    # TODO: make visualisation work with functions from getFormulaString
+    # visualisation(function_string_parser(getFormulaString()), 6)
+
 
 if __name__ == "__main__":
     main()
