3cc5e56bb969cd9650b96aa6f937a27fbef641b6
DawidKahla <dawid.kahla@onet.pl>
2021-05-13 00:13:08 +0200

    UI added
    Not working first attempt to interface

--- main.py
+++ main.py
@@ -4,14 +4,20 @@ from goldstein import goldstein
 import sympy as sp
 from mns import mns
 from functionStringParser import function_string_parser
-
-
+from ui.ui_first_attempt import Ui_MainWindow
+from PyQt5 import QtWidgets
 def main():
-    formula_string = getFormulaString()
-    # goldstein(sp.Matrix([0, 0]), sp.Matrix([1, 0]), 2/5, 1, 0.001, formula_string)
-    mns(sp.Matrix([4, 8]), 0.0001, 1000, 1 / 4, 3, formula_string)
-    visualisation(function_string_parser(formula_string), 6)
-
+    # formula_string = getFormulaString()
+    # mns(sp.Matrix([-1, -1]), 0.0001, 1000, 1 / 4, 3, formula_string)
+    # visualisation(function_string_parser(formula_string), 6)
+    return 0;
 
 if __name__ == "__main__":
-    main()
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
