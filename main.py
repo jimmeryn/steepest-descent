36c3351a62935342679ba64265f76096230d885d
jimmeryn <jimmeryn@gmail.com>
2021-05-13 15:11:18 +0200

    main fix 2

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-from getFormulaString import getFormulaString
-from visualisation import visualisation
-from goldstein import goldstein
-import sympy as sp
-from mns import mns
-from functionStringParser import function_string_parser
-from ui.ui import MainWindow
-from PyQt5 import QtWidgets as qtw
 import sys
-from ui.ui_first_attempt import Ui_MainWindow
 from PyQt5 import QtWidgets
+from ui.ui import MainWindow as UiWindow
 from ui.gui import MainWindow as GuiWindow
 
 
@@ -16,22 +8,14 @@ def main():
     a = 2
     if a == 1:
         app = QtWidgets.QApplication(sys.argv)
-        MainWindow = QtWidgets.QMainWindow()
-        ui = Ui_MainWindow()
-        ui.setupUi(MainWindow)
-        MainWindow.show()
-        sys.exit(app.exec_())
-
-    if a == 2:
-        app = QtWidgets.QApplication(sys.argv)
         app.setStyle("Fusion")
         window = GuiWindow()
         window.show()
         sys.exit(app.exec_())
 
-    if a == 3:
-        app = qtw.QApplication(sys.argv)
-        win = MainWindow()
+    if a == 2:
+        app = QtWidgets.QApplication(sys.argv)
+        win = UiWindow()
         win.show()
         sys.exit(app.exec_())
 
