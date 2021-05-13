13818008a59feb6ded50efd5d24e253f3a5d3c49
DawidKahla <dawid.kahla@onet.pl>
2021-05-13 12:33:32 +0200

    Showing primitive UI

--- main.py
+++ main.py
@@ -4,20 +4,17 @@ from goldstein import goldstein
 import sympy as sp
 from mns import mns
 from functionStringParser import function_string_parser
-from ui.ui_first_attempt import Ui_MainWindow
-from PyQt5 import QtWidgets
+from ui.ui import MainWindow
+from PyQt5 import QtWidgets as qtw
+import sys
 def main():
     # formula_string = getFormulaString()
     # mns(sp.Matrix([-1, -1]), 0.0001, 1000, 1 / 4, 3, formula_string)
     # visualisation(function_string_parser(formula_string), 6)
-    return 0;
+    app = qtw.QApplication(sys.argv)
+    win = MainWindow()
+    win.show()
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
-    import sys
-
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
+    main()
