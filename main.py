8a92e8270bbc228595ada11830742f901b94e18b
jimmeryn <jimmeryn@gmail.com>
2021-05-16 20:46:42 +0200

    deleted gui.py

--- main.py
+++ main.py
@@ -1,25 +1,14 @@
 import sys
 from PyQt5 import QtWidgets
 from ui.ui import MainWindow as UiWindow
-from ui.gui import MainWindow as GuiWindow
 
 
 def main():
-    a = 2
-    if a == 1:
-        app = QtWidgets.QApplication(sys.argv)
-        app.setStyle("Fusion")
-        window = GuiWindow()
-        window.show()
-        sys.exit(app.exec_())
-
-    if a == 2:
-        app = QtWidgets.QApplication(sys.argv)
-        win = UiWindow()
-        win.show()
-        sys.exit(app.exec_())
-
-    return 0
+    app = QtWidgets.QApplication(sys.argv)
+    app.setStyle("Fusion")
+    win = UiWindow()
+    win.show()
+    sys.exit(app.exec_())
 
 
 if __name__ == "__main__":
